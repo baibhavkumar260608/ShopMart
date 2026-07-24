ae5fcb62fc9e2966e7e1e83d1b0a055c113ff820
baibhavkumar260608 <80514132+baibhavkumar260608@users.noreply.github.com>
2026-07-25 00:26:52 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -3,13 +3,9 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
 
     host: process.env.MYSQL_HOST,
-
     user: process.env.MYSQL_USER,
-
     password: process.env.MYSQL_PASSWORD,
-
     database: process.env.MYSQL_DATABASE,
-
     port: Number(process.env.MYSQL_PORT),
 
     ssl:{
@@ -17,15 +13,13 @@ const pool = mysql.createPool({
     },
 
     waitForConnections:true,
-
     connectionLimit:10,
-
     queueLimit:0
 
 });
 
 
-pool.getConnection((err, connection)=>{
+pool.getConnection((err,connection)=>{
 
     if(err)
     {
